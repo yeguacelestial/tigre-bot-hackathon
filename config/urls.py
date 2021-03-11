bc11003826ea94169554cf071e12e593775e0d1d
Yegua Celestial <yeguacelestial@gmail.com>
2021-03-11 16:41:02 -0600

    Update base endpoint

--- config/urls.py
+++ config/urls.py
@@ -16,17 +16,9 @@ Including another URLconf
 from django.contrib import admin
 
 from django.urls import include, path
-from rest_framework import routers
-from chatbot import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/v1/', include('chatbot.urls')),
 ]
