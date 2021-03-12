57acdcd9689237067bb60559c5333911a6bf0542
Yegua Celestial <yeguacelestial@gmail.com>
2021-03-11 20:23:46 -0600

    Add Heroku files

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+import django_heroku
+django_heroku.settings(locals())
