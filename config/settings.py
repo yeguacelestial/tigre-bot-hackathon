db5be69f52a5fdf6f22c5ef3303620fa3ffef821
Yegua Celestial <yeguacelestial@gmail.com>
2021-03-11 23:36:57 -0600

    Remove CORS headers

--- config/settings.py
+++ config/settings.py
@@ -30,23 +30,6 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'access-control-allow-origin',
-]
-
-CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-]
 
 # Application definition
 
